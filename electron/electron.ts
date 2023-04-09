6ddc7b402a15e4ffaf6b6eb22a9c6195ebb3fffb
newcat <freddy.wagner@web.de>
2023-04-09 20:40:29 +0200

    Add Vue devtools

--- electron/electron.ts
+++ electron/electron.ts
@@ -1,5 +1,6 @@
 import * as path from "path";
 import { app, BrowserWindow, dialog, ipcMain, OpenDialogOptions, SaveDialogOptions } from "electron";
+import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
 
 import "./serialport";
 
@@ -62,6 +63,10 @@ app.whenReady().then(() => {
         // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
+
+    installExtension(VUEJS3_DEVTOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log("An error occurred: ", err));
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
