553be7ec588f1a192f08ebc36f59188fe6facd4d
newcat <freddy.wagner@web.de>
2023-04-17 02:11:39 +0200

    Fix peak node calculation

--- src/graph/nodes/input/PeakNode.ts
+++ src/graph/nodes/input/PeakNode.ts
@@ -15,14 +15,10 @@ export const PeakNode = defineNode({
         peak: () => new NumberInterface("Peak", 0),
     },
     calculate({ minDb, maxDb }, context: LmsCalculationContext) {
-        const { timeDomainData, sampleRate } = context.globalValues;
-        const sampleSize = Math.max(timeDomainData.length, (sampleRate / 1000) * 30);
-        let sum = 0;
-        for (let i = timeDomainData.length - sampleSize; i < timeDomainData.length; i++) {
-            sum += timeDomainData[i] * timeDomainData[i];
-        }
-        const rms = Math.sqrt(sum / (sampleSize / 2));
-        const db = 20 * Math.log10(rms);
+        const { timeDomainData, sampleRate, fps } = context.globalValues;
+        const sampleSize = Math.min(timeDomainData.length, (sampleRate / 1000) * fps);
+        const max = Math.max(...timeDomainData.slice(-sampleSize));
+        const db = 20 * Math.log10(Math.sqrt(max));
         const v = Math.min(1, Math.max(0, (db - minDb) / (maxDb - minDb)));
 
         return { preview: v, peak: v };
