8774975dba2694fcc074406ee4c59a7540eb2e6a
newcat <freddy.wagner@web.de>
2024-01-06 14:33:02 +0100

    Try GH pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.ELECTRON == "true" ? "./" : "",
+    base: process.env.ELECTRON == "true" ? "./" : process.env.BASE_URL ?? "",
     server: {
         port: 3000,
     },
